8d6fae0abed4eecc2c458e70ce56b2b5efc24226
Victor Fernandes <victor.hmfd@gmail.com>
2019-03-04 10:34:09 -0300

    initial commit
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
